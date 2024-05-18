d373d304d138463ac568ece4777472b490c931f5
Emmanuel Adebiyi <adebiyiemmanuel11@gmail.com>
2024-05-18 17:18:51 +0100

    api

--- utils/index.ts
+++ utils/index.ts
@@ -4,7 +4,7 @@ export async function fetchcars( filters: FilterProps) {
   const { manufacturer, year, model, limit, fuel } = filters;
 
   const headers = {
-    'X-RapidAPI-Key':process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
+    'X-RapidAPI-Key':process.env.NEXT_PUBLIC_RAPID_API_KEY!,
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
   }
 
